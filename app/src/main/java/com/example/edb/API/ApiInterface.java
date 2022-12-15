733b91bd29adbf4ff4cc4ef907dbb2d603f976c2
Salah Ashraf <salahashraf924@gmail.com>
2022-12-15 20:29:15 +0200

    Update ApiInterface.java

--- app/src/main/java/com/example/edb/API/ApiInterface.java
+++ app/src/main/java/com/example/edb/API/ApiInterface.java
@@ -1,26 +1,28 @@
 package com.example.edb.API;
 
 import com.example.edb.Model.User;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 public interface ApiInterface {
 
     @POST("login")
     Call<User> getUserInfo(@Body User user);
-    
+
     @POST("login")
-    Call<User> getUserInfo(@Body HashMap<String,String>map);
+    Call<User> getUserInfo(@Body HashMap<String, String> map);
+
+    @POST("add-user")
+    Call<User> addUser(@Body User user);
+
+    @POST("add-transaction")
+    Call<User> addTransaction(@Body User user);
+
+    @POST("update-balance")
+    Call<User> updateBalance(@Body User user);
+
 }
