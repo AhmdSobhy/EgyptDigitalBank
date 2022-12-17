010f53ab9334c5b883ca24144064edb043b2e5f7
Ahmed Sobhy <a7mdsobhy@gmail.com>
2022-12-17 18:59:38 +0200

    Add Biometric Login

--- app/src/main/java/com/example/edb/LoginActivity.java
+++ app/src/main/java/com/example/edb/LoginActivity.java
@@ -2,13 +2,20 @@ package com.example.edb;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.edb.API.ApiInterface;
@@ -27,6 +34,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.concurrent.Executor;
 
 import cz.msebera.android.httpclient.entity.StringEntity;
 
@@ -39,6 +47,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,43 +58,23 @@ public class LoginActivity extends AppCompatActivity {
 
         EditText emailTxt = findViewById(R.id.email_txt);
         EditText passwordTxt = findViewById(R.id.password_txt);
-        Button LoginBtn = findViewById(R.id.login_acc_btn);
+        Button loginBtn = findViewById(R.id.login_acc_btn);
+        ImageButton fingerprintLoginBtn = findViewById(R.id.fingerprint_btn);
         Button createAccountBtn = findViewById(R.id.login_create_acc_btn);
 
-        LoginBtn.setOnClickListener(new View.OnClickListener() {
+        sharedPreferences = getSharedPreferences("userdata", MODE_PRIVATE);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String cloudDbUrl = "https://bank-db-api.herokuapp.com/";
+                loginFromAPI(emailTxt.getText().toString(), passwordTxt.getText().toString());
+            }
+        });
 
-                Retrofit retrofit=new Retrofit.Builder().baseUrl(cloudDbUrl).addConverterFactory(GsonConverterFactory.create()).build();
-                ApiInterface apiInterface=retrofit.create(ApiInterface.class);
-
-                HashMap<String, String> map = new HashMap<>();
-                map.put("Email",emailTxt.getText().toString());
-                map.put("Password", passwordTxt.getText().toString());
-
-                Call<User> call= apiInterface.getUserInfo(map);
-
-                call.enqueue(new Callback<User>() {
-                    @Override
-                    public void onResponse(Call<User> call, @NonNull Response<User> response) {
-                        if(response.code()== 200){
-                            Toast.makeText(LoginActivity.this, "You have log in successfully ::)", Toast.LENGTH_LONG).show();
-                            User user = response.body();
-                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                            i.putExtra("user",user);
-                            startActivity(i);
-                        }
-                        else{
-                            Toast.makeText(LoginActivity.this, "Wrong Email or Password", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                    @Override
-                    public void onFailure(Call<User> call, Throwable t) {
-                        Toast.makeText(LoginActivity.this, "Error at internet connection", Toast.LENGTH_LONG).show();
-                    }
-                });
-
+        fingerprintLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginWithFingerprint();
             }
         });
 
@@ -93,5 +85,95 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    void loginFromAPI(String email, String password) {
+        String cloudDbUrl = "https://bank-db-api.herokuapp.com/";
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(cloudDbUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        ApiInterface apiInterface = retrofit.create(ApiInterface.class);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Email", email);
+        map.put("Password", password);
+
+        Call<User> call = apiInterface.getUserInfo(map);
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, @NonNull Response<User> response) {
+                if (response.code() == 200) {
+                    Toast.makeText(LoginActivity.this, "You have log in successfully ::)", Toast.LENGTH_LONG).show();
+                    User user = response.body();
+                    sharedPreferences.edit().putString("email", user.getEmail()).commit();
+                    sharedPreferences.edit().putString("password", user.getPassword()).commit();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    i.putExtra("user", user);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Wrong Email or Password", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "Error at internet connection", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    void loginWithFingerprint() {
+        BiometricManager biometricManager = BiometricManager.from(this);
+
+        switch (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(getApplicationContext(), "Device doesn't have fingerprint", Toast.LENGTH_SHORT).show();
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(getApplicationContext(), "fingerprint is not working", Toast.LENGTH_SHORT).show();
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(getApplicationContext(), "No fingerprint registered", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                String email = sharedPreferences.getString("email", "null");
+                String password = sharedPreferences.getString("password", "null");
+
+                if (!email.equals("null") && !password.equals("null"))
+                    loginFromAPI(email, password);
+                else
+                    Toast.makeText(getApplicationContext(), "Login to enable fingerprint", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Wrong email or password", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setDescription("Use fingerprint to login")
+                .setNegativeButtonText("Use Password")
+                .setConfirmationRequired(false)
+                .build();
+
+        biometricPrompt.authenticate(promptInfo);
     }
 }
