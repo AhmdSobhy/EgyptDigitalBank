0bfffcddc446903f35dbc5399988b687bc139aef
Salah Ashraf <salahashraf924@gmail.com>
2022-12-17 14:10:06 +0200

    change update balance http method from post to patch

--- app/src/main/java/com/example/edb/API/ApiInterface.java
+++ app/src/main/java/com/example/edb/API/ApiInterface.java
@@ -6,7 +6,10 @@ import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
 
@@ -22,7 +25,7 @@ public interface ApiInterface {
     @POST("add-transaction")
     Call<User> addTransaction(@Body User user);
 
-    @POST("update-balance")
-    Call<User> updateBalance(@Body User user);
+    @PATCH("update-balance/{userSSN}/{accountId}")
+    Call<Void> updateBalance(@Path("userSSN") String userSSN, @Path("accountId")String accountId, @Body HashMap<String,String>map);
 
 }
