12ff51a1311126728e84db8aaf3127aa2ef2e884
Salah Ashraf <salahashraf924@gmail.com>
2022-12-27 23:28:49 +0200

    create function add Transaction

--- app/src/main/java/com/example/edb/Model/Account.java
+++ app/src/main/java/com/example/edb/Model/Account.java
@@ -106,4 +106,8 @@ public class Account implements Serializable {
     public void set_id(String _id) {
         this._id = _id;
     }
+
+    public void addTransaction(Transaction transaction){
+        this.Transactions.add(transaction);
+    }
 }
